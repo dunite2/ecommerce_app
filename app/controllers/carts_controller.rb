52950374116a37f7e754c046150c6d3777419b0d
dunite2 <dunite2@rrc.ca>
2025-08-07 23:37:55 -0500

    validations

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,28 +4,81 @@ class CartsController < ApplicationController
   end
 
   def add_item
-    product = Product.find(params[:product_id])
+    product = Product.find_by(id: params[:product_id])
+    
+    unless product
+      flash[:alert] = "Product not found."
+      redirect_to request.referer || root_path
+      return
+    end
+    
+    unless product.in_stock?
+      flash[:alert] = "#{product.name} is currently out of stock."
+      redirect_to request.referer || root_path
+      return
+    end
+    
     quantity = params[:quantity]&.to_i || 1
     
-    current_cart.add_product(product.id, quantity)
-    save_cart
+    unless quantity > 0 && quantity <= 100
+      flash[:alert] = "Invalid quantity. Please select between 1 and 100."
+      redirect_to request.referer || root_path
+      return
+    end
     
-    redirect_to request.referer || root_path, notice: "#{product.name} added to cart!"
+    if current_cart.add_product(product.id, quantity)
+      save_cart
+      flash[:notice] = "#{product.name} added to cart!"
+    else
+      flash[:alert] = "Could not add #{product.name} to cart. Please check stock availability."
+    end
+    
+    redirect_to request.referer || root_path
   end
 
   def update_item
-    current_cart.update_quantity(params[:product_id], params[:quantity])
-    save_cart
+    product = Product.find_by(id: params[:product_id])
+    quantity = params[:quantity]&.to_i || 0
     
-    redirect_to cart_path, notice: "Cart updated!"
+    unless product
+      flash[:alert] = "Product not found."
+      redirect_to cart_path
+      return
+    end
+    
+    unless quantity >= 0 && quantity <= 100
+      flash[:alert] = "Invalid quantity. Please select between 0 and 100."
+      redirect_to cart_path
+      return
+    end
+    
+    if current_cart.update_quantity(product.id, quantity)
+      save_cart
+      flash[:notice] = quantity > 0 ? "Cart updated!" : "#{product.name} removed from cart!"
+    else
+      flash[:alert] = "Could not update cart. Please check stock availability."
+    end
+    
+    redirect_to cart_path
   end
 
   def remove_item
-    product = Product.find(params[:product_id])
-    current_cart.remove_product(product.id)
-    save_cart
+    product = Product.find_by(id: params[:product_id])
     
-    redirect_to cart_path, notice: "#{product.name} removed from cart!"
+    unless product
+      flash[:alert] = "Product not found."
+      redirect_to cart_path
+      return
+    end
+    
+    if current_cart.remove_product(product.id)
+      save_cart
+      flash[:notice] = "#{product.name} removed from cart!"
+    else
+      flash[:alert] = "Could not remove #{product.name} from cart."
+    end
+    
+    redirect_to cart_path
   end
 
   def clear
